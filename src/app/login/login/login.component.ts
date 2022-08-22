06518ace36d68141afe8c080f65f2981bfea345e
nicamiranda <109951283+nicamiranda@users.noreply.github.com>
2022-08-22 15:41:24 +0800

    Update login.component.ts

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -17,7 +17,7 @@ export class LoginComponent implements OnInit {
   hide: boolean = false;
   users : any = [];
   responsedata: any;
-  loginurl = `${environment.url}/login`;
+  loginurl = `${https://json-server-seven-mock.herokuapp.com}/login`;
 
   constructor(private lf : FormBuilder,
               private router: Router,
